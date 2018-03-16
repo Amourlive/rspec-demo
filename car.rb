9e2f21bef9d4483ddc5be16654c1942d2262cd0c
Amourlive <aleksei_mackevich@outlook.com>
2018-03-16 14:14:47 +0300

    Remove method at SuperClass

--- car.rb
+++ car.rb
@@ -1,20 +1,4 @@
-class Car #:nodoc:
-  MILES_PER_GALLON = 20
-
-  attr_reader :fuel
-
-  def initialize
-    @fuel = 0
-  end
-
-  def add_fuel(amount)
-    @fuel += amount
-  end
-
-  # How far can we go?
-  def range
-    @fuel * MILES_PER_GALLON
-  end
+class Car < Vehicle #:nodoc:
 end
 
 # car = Car.new
