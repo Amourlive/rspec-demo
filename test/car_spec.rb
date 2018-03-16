52c830df0a6ee5e1ef1949ceac5721aec86f13f1
Amourlive <aleksei_mackevich@outlook.com>
2018-03-16 15:53:47 +0300

    refactoring test

--- test/car_spec.rb
+++ test/car_spec.rb
@@ -3,12 +3,14 @@ require 'rspec'
 require '../car'
 
 describe Car do
-  it 'should Test car class' do
+  before do
     # arrange
-    car = Car.new
+    @car = Car.new
     # act
-    car.add_fuel 10
-    # assert
-    expect(car.range).to eq 200
+    @car.add_fuel 10
+  end
+  # assert
+  it 'should Test car class' do
+    expect(@car.range).to eq 200
   end
 end
